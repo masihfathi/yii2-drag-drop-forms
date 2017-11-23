fc71bf649ac4b6cfef0173442915c50b7380b79e
masihfathi <masihfathi@gmail.com>
2017-11-24 00:05:36 +0330

    fix namespace

--- FormAsset.php
+++ FormAsset.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
+
 namespace masihfathi\form;
 
 class FormAsset extends \yii\web\AssetBundle {
-    public $sourcePath = '@app/modules/admin/modules/form/assets/form';
+
+    public $sourcePath = '@masihfathi/form/assets/form';
     public $baseUrl = '@web';
     public $js = [
         'js/forms/helpers.js',
         'js/forms/form.js',
         'js/forms/field.js',
- //       'js/forms/form.min.js',
+            //       'js/forms/form.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
     ];
+
 }
-
-
